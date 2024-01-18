66506e9d672491765e9e0694ae9e89b43753f5e4
Ashok-Prajapati2 <ak4132592ashok@gmail.com>
2024-01-18 19:15:40 +0530

    Constructors and inhritance

---
+++ opps/07.cpp
@@ -0,0 +1,35 @@
+//  Constructors and inheritance
+#include<iostream>
+using namespace std;
+
+class test_base 
+{
+ public:
+
+ test_base(){
+    cout << "test_base(), defult constructor" << endl;
+ }
+ test_base(int a){
+    cout << "test_base(),paramatric  constructor : " << a << endl;
+ }
+};
+
+class test_child : public test_base
+{
+public:
+test_child() : test_base()
+    {
+    cout << "test_child(),defult constructor" << endl;
+    }
+test_child(int b) : test_base(b)
+    {
+    cout << "test_child(),paramatric  constructor : " << b << endl;
+    }
+};
+
+int main(){
+    test_child a;  
+    cout << "<< " << endl;
+    test_child a1(5);  
+    return 0;
+}
--- opps/tempCodeRunnerFile.cpp
+++
@@ -1,7 +0,0 @@
-Person(int x) // parameterized constructor
-    // {
-    //     name = "null";
-    //     age = 0;
-    //     height = 0.0f;
-    //     parameterized_constructor = x;
-    // }
